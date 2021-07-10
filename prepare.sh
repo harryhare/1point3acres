0f56fffad917da6b87755ce85ef6c1e736a0e49a
harryhare <harryhare@163.com>
2021-07-11 05:02:59 +0800

    update circle ci

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo apt-get -y install tesseract-ocr=4.1.1-2build2
+sudo apt-get -y install python3 python3-pip tesseract-ocr=4.1.1-2build2
 #apt-cache policy  tesseract-ocr
 pip3 install -r requirements.txt
