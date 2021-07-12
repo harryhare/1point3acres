1ea436f1e33d8e64a07008ed29a49f166bd6c50f
harryhare <harryhare@163.com>
2021-07-12 16:06:11 +0800

    disable github action

--- src/get_cf_clearance.py
+++ src/get_cf_clearance.py
@@ -2,16 +2,17 @@ import json
 import http.cookies
 
 def get_basic_cookie():
-	f = open("../flaresolverr.json", "rb")  # gbk 编码要用 tb 打开
-	data = json.load(f)
-	f.close()
-	cookies = data['solution']['cookies']
-	cookie_lines=[]
-	for c in cookies:
-		k=c["name"]
-		v=c["value"]
-		cookie_lines.append(f'{k}={v}')
-	raw_cookie_line = "; ".join(cookie_lines)
+	# f = open("../flaresolverr.json", "rb")  # gbk 编码要用 tb 打开
+	# data = json.load(f)
+	# f.close()
+	# cookies = data['solution']['cookies']
+	# cookie_lines=[]
+	# for c in cookies:
+	# 	k=c["name"]
+	# 	v=c["value"]
+	# 	cookie_lines.append(f'{k}={v}')
+	# raw_cookie_line = "; ".join(cookie_lines)
+	raw_cookie_line="cf_clearance=60850fb6ffd4f2d1e0f81d54dbe93024eefca053-1626071331-0-150"
 	simple_cookie = http.cookies.SimpleCookie(raw_cookie_line)
 	return simple_cookie
 
